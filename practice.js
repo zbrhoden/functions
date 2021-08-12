cb325fbea77d1cf55fdd6e34014a7ee23be0b23f
Blake Rhoden <zbrhoden@gmail.com>
2021-08-12 13:25:22 -0500

    functions

---
+++ practice.js
@@ -0,0 +1,7 @@
+const calculateAgeInDogYears = (ageOfPerson) => {
+    const ageInDogYears = ageOfPerson / 7
+
+    return ageInDogYears
+}
+const dogAge = calculateAgeInDogYears(45)
+console.log(dogAge)
